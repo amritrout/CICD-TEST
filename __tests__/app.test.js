ddc866fd4e03ff7c50b007999ceec5af452d1340
Ghostfury73 <amritrout73@gmail.com>
2024-09-28 23:14:17 +0530

    test added

---
+++ __tests__/app.test.js
@@ -0,0 +1,38 @@
+const http = require('http');
+const serverModule = require('../server');
+
+describe('HTTP Server', function() {
+    let serverInstance;
+
+    beforeAll(function(done) {
+        serverInstance = serverModule.startServer();
+        done();
+    });
+
+    afterAll(function(done) {
+        if (serverInstance) {
+            serverInstance.close(done);
+        } else {
+            done(new Error('Server instance is undefined'));
+        }
+    });
+
+    test('responds with Hello, World!', function(done) {
+        http.get('http://localhost:3000', function(res) {
+            let data = '';
+
+            res.on('data', function(chunk) {
+                data += chunk;
+            });
+
+            res.on('end', function() {
+                expect(res.statusCode).toBe(200);
+                expect(data).toBe('Hello, World! its me mariooo\n');
+                done();
+            });
+        }).on('error', (err) => {
+            console.error('Request failed:', err);
+            done(err);
+        });
+    });
+});
--- server.js
+++ server.js
@@ -9,6 +9,13 @@ const server = http.createServer((req, res) => {
     res.end('Hello, World! its me mariooo\n');
 });
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+const startServer = () => {
+    return server.listen(port, hostname, () => {
+        console.log(`Server running at http://${hostname}:${port}/`);
+    }).on('error', (err) => {
+        console.error('Server encountered an error:', err);
+    });
+};
+
+startServer();
+module.exports = { startServer, server };
